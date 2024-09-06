d6d7a7ba7b11f483bfc346eb6a953eacff1249a2
unknown <davidarthurforbes@gmail.com>
2024-09-05 21:22:04 -0400

    studio complete

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -2,16 +2,32 @@ package org.launchcode;
 
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class Area {
     public static void main(String[] args){
-        double radius;
+        double radius = 0;
         Scanner input;
-        input = new Scanner(System.in);
-        System.out.println("Enter the radius of a circle. please.");
-        radius = input.nextDouble();
-        input.close();
+        input = new Scanner(in);
 
-        double area = Circle.getArea(radius);//(3.1415926) * radius * radius;
-        System.out.println("The area of a circle with radius "+ radius+"is: "+ area);
+        try {
+            System.out.println("Enter the radius of a circle. please.");
+            radius = input.nextDouble();
+            input.close();
+        } catch (Exception e) {
+            System.out.println("Radius must be a number");
+            System.exit(0);
+        };
+
+        if (radius > 0) {
+            double area = Circle.getArea(radius);//(3.1415926) * radius * radius;
+            System.out.println("The area of a circle with radius "+ radius+"is: "+ area);
+        } else {
+            System.out.println("Radius must be a positive number.");
+
+        }
+
+
+
     }
 }
